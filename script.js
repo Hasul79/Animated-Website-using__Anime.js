7a24d98d69434587cea5d450dec4746dc6e345f0
Hasul79 <hatuk2@mail.ru>
2022-10-23 22:32:10 +0400

    add anime.js

---
+++ script.js
@@ -0,0 +1,42 @@
+let container = document.querySelector('.container');
+for(let i = 1; i <= 100; i++){
+    let dot = document.createElement('div');
+    dot.classList.add('element');
+    container.appendChild(dot);
+}
+
+let dotAll = document.querySelectorAll('.element');
+let animation = anime.timeline({
+    targets: dotAll,
+    easing: 'easeInOutExpo',
+    loop: true,
+    delay: anime.stagger(100, {grid: [10,10], from: 'center'})
+})
+animation
+.add({
+    rotateZ: 180,
+    translateY: anime.stagger(-20, {grid: [10,10], from: 'center', axis: 'y'}),
+    translateX: anime.stagger(-20, {grid: [10,10], from: 'center', axis: 'x'}),
+    opacity: 1,
+})
+.add({
+    borderRadius: 50,
+})
+.add({
+    scale: 0.2,
+    opacity: 0.2,
+})
+animation
+.add({
+    rotateZ: 180,
+    translateY: anime.stagger(0, {grid: [10,10], from: 'center', axis: 'y'}),
+    translateX: anime.stagger(0, {grid: [10,10], from: 'center', axis: 'x'}),
+    opacity: 1,
+})
+.add({
+    scale: 1,
+    borderRadius: 0,
+})
+.add({
+   rotateZ: -90,
+})
